d1a00a29f5bdde027bd7de65a6c21c8924ea92f3
pvenkateshnaidu <pvenkateshnaidu@gmail.com>
2019-07-05 19:02:48 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, HeaderComponent, FooterComponent, HomeComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
